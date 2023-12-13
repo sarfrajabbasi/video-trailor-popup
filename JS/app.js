fe0fea31f4f80375d860b86e3db1710f28537487
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-13 11:04:20 +0530

    video trailer

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,16 @@
-const mainContainer = document.cre
+function createEl(e){
+    const element  = document.createElement(e);
+    return element
+}
+const MAINCONTAINER = createEl('div');
+MAINCONTAINER.classList.add("main-container");
+
+const IMG = createEl('img');
+IMG.src = 'abx';
+IMG.alt = 'movie-image';
+
+
+// append
+MAINCONTAINER.appendChild(IMG);
+
+document.body.appendChild(MAINCONTAINER)
