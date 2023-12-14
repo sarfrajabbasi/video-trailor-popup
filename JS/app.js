186baaacac6d0f92387a76303bb236aac3410b0e
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-14 12:41:50 +0530

    video trailer

--- JS/app.js
+++ JS/app.js
@@ -9,16 +9,16 @@ function generaterandomWords(words) {
   // Split the Lorem Ipsum text into an array of words
   const loremWords = loremText.split(" ");
   // Take the specified number of words from the array
-  
+
   let newArr = [];
-  let randomWord = ()=>{
-   return loremWords[Math.floor((Math.random()*loremWords.length))]
-  }
-  for(let i=1;i<=words;i++){
+  let randomWord = () => {
+    return loremWords[Math.floor(Math.random() * loremWords.length)];
+  };
+  for (let i = 1; i <= words; i++) {
     //   console.log(randomWord());
-    newArr.push(randomWord())
+    newArr.push(randomWord());
   }
-//   console.log(newArr.join(' '));
+  //   console.log(newArr.join(' '));
 
   // Join the selected words back into a string
   return newArr.join(" ");
@@ -37,27 +37,45 @@ movieName.textContent = "movie Title";
 const para = createEl("p");
 para.innerHTML = generaterandomWords(100);
 
-const btn = createEl('button');
-btn.classList.add('btn')
-btn.textContent = 'Watch now';
+const btn = createEl("button");
+btn.classList.add("btn");
+btn.textContent = "Watch now";
 
 // trailer
-const trailer = createEl('div');
-trailer.classList.add('trailer-container');
+const trailer = createEl("div");
+trailer.classList.add("trailer-container");
+trailer.classList.add("hide");
 
-const video = createEl('video');
+const video = createEl("video");
 video.src = "./video/pexels-sunsetoned-5968893 (2160p).mp4";
-video.setAttribute('controls','true');
+video.setAttribute("controls", "true");
 
-const close1 = createEl('i');
-const classToAdd = 'fas fa-times fa-2x' 
-close1.classList.add(...classToAdd.split(' '))
+const close1 = createEl("i");
+const classToAdd = "fas fa-times fa-2x close-icon";
+close1.classList.add(...classToAdd.split(" "));
 
 // append
-trailer.append(video,close1);
-MAINCONTAINER.append(IMG,movieName,para,btn);
+trailer.append(video, close1);
+MAINCONTAINER.append(IMG, movieName, para, btn);
 
-document.body.append(MAINCONTAINER,trailer);
+document.body.append(MAINCONTAINER, trailer);
 
-// log
-console.log(close1);
+// close the video and open
+
+// select element
+const btnClass = document.querySelector(".btn");
+const closeIcon1 = document.querySelector(".close-icon");
+const video2 = document.querySelector("video");
+
+const trailerContainer = document.querySelector(".trailer-container");
+
+// event
+btnClass.addEventListener("click", () => {
+  trailerContainer.classList.remove("active");
+});
+
+closeIcon1.addEventListener("click", () => {
+  trailerContainer.classList.add("active");
+  video2.pause();
+  video2.currentTime = 0;
+});
